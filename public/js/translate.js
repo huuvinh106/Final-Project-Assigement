0e472c5b8cb225b78e8c0b47e9bf5f83a763452c
huuvinh106 <buihuuvinh106@gmail.com>
2017-03-20 12:19:16 +0700

    update file n20

--- public/js/translate.js
+++ public/js/translate.js
@@ -1,12 +1,20 @@
-var ng = angular.module('ng', []);
+var ng = angular.module('ng', ['pascalprecht.translate']);
 ng.config(['$translateProvider', function($translateProvider) {
-    $translateProvider
-        .useStaticFilesLoader({
-            prefix: '/translations/',
-            suffix: '.json'
-        })
-        .preferredLanguage('ar')
-        .useMissingTranslationHandlerLog();
+    $translateProvider.fallbackLanguage('en');
+    $translateProvider.registerAvailableLanguageKeys(['en', 'vi', {
+        'en_*': 'en',
+    }])
+    $translateProvider.translations('en', {
+        BUTTON_LANG_EN: "english"
+
+    });
+    // $translateProvider
+    //     .useStaticFilesLoader({
+    //         prefix: '/translations/',
+    //         suffix: '.json'
+    //     })
+    //     .preferredLanguage('ar')
+    //     .useMissingTranslationHandlerLog();
 }]);
 var lang = ng.controller('lang', ['$scope', '$translate', function($scope, $translate) {
     $scope.changeLanguage = function(key) {
